77868c95d6f10405ac020b74461f4b136165f5ae
Ingeborg Adolfs <ingeborgadolfs@gmail.com>
2024-12-26 19:23:46 -0700

    first commit

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,10 @@
+import { NavLink } from "react-router-dom"
+
+export default function NavBar(){
+    return(
+        <nav className="navbar">
+        <NavLink to="/" className="navbar">Home</NavLink>
+        <NavLink to="/photographers" className="navbar">Photographers</NavLink>
+      </nav>
+    )
+}
---
+++ src/pages/Photographers.jsx
@@ -0,0 +1,5 @@
+export default function Photographers(){
+    return(
+        console.log("Photographers")
+    )
+}
