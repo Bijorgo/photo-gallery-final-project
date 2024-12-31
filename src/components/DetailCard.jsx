4ef332c4a6cb685e5bd073f48e6252187d5d6d2b
Ingeborg Adolfs <ingeborgadolfs@gmail.com>
2024-12-31 16:45:42 -0700

    style details

--- src/components/DetailCard.jsx
+++ src/components/DetailCard.jsx
@@ -1,11 +1,27 @@
 export default function DetailCard( {details} ){
     return(
-        <div>
-           <img src={details.url} alt={details.alt}/>
-            <h4>{details.detail}</h4>
-            <h3>Location: {details.location}</h3>
-            <h5>{details.date}</h5>
-            <h6>Photograph by: {details.photographer}</h6>
+        <div className="min-h-screen bg-backgroundDark py-12 px-6">
+            <h3 className="text-neonPink">
+                Location: {details.location}
+            </h3>
+
+            <img 
+                src={details.url} 
+                alt={details.alt}
+                className="w-full h-96 object-contain rounded-lg mb-6"
+            />
+
+            <h4 className="text-3xl font-bold text-darkBlue">
+                {details.detail}
+            </h4>
+
+            <h5 className="text-lg text-neonPurple mb-2">
+                {details.date}
+            </h5>
+
+            <h6 className="text-lg text-neonPink">
+                Photograph by: {details.photographer}
+            </h6>
         </div>
     )
 }
