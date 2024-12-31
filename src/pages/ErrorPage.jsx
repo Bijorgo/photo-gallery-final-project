a6af01e832c26fc2bf08bc9d52106789a4e2d649
Ingeborg Adolfs <ingeborgadolfs@gmail.com>
2024-12-31 16:20:07 -0700

    style error page

--- src/pages/ErrorPage.jsx
+++ src/pages/ErrorPage.jsx
@@ -5,9 +5,21 @@ export default function ErrorPage(){
     const error = useRouteError();
     console.log(error);
     return(
-        <div>
-            <h1>Ruh roh, something went wrong!</h1>
-            <Link to="/"> HOME </Link>
+        <div className="min-h-screen text-white flex flex-col justify-center items-center py-12 px-6">
+            <h1 className="text-6xl font-bold text-center text-neonPink mb-8 hover:text-neonCyan hover:animate-neonGlow transition-all">
+                Ruh roh, something went wrong!
+            </h1>
+
+            <p className="text-xl mb-6 text-center text-neonGreen">
+                Don't worry, let's get you back to safety.
+            </p>
+
+            <Link
+                to="/"
+                className="text-xl bg-neonYellow text-darkBlue py-2 px-6 rounded-full glowButton hover:animate-neonGlow transition-all"
+            >
+                HOME
+            </Link>
         </div>
     )
 }
